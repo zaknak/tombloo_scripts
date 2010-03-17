f81fbf8f6331be5b1249f41949ee9addf19c8895
zaknak <zaknak75@gmail.com>
2010-03-17 21:40:59 +0900

    実行場所に適したタイプでの即時ポストに失敗していたのを修正
    即時ポスト時には存在しない値を使用していた為。

--- scripts/extractor.pixiv.js
+++ scripts/extractor.pixiv.js
@@ -1,9 +1,9 @@
 Tombloo.Service.extractors.register([
 	{
-		name : 'photo - pxiv',
-		ICON : 'chrome://tombloo/skin/photo.png',
+		name : 'Photo - pxiv',
+		ICON : 'http://www.pixiv.net/favicon.ico',
 		check : function(ctx){
-			return ctx.onImage && ctx.imageURL.match(/^http:\/\/img\d.+?\.pixiv\.net\/img\//);
+			return ctx.onImage && ctx.target.src.match(/^http:\/\/img\d.+?\.pixiv\.net\/img\//);
 		},
 		extract : function(ctx){
 			illustURL = /^http:\/\/www\.pixiv\.net\/member_illust.php\?mode=/;
@@ -12,14 +12,14 @@ Tombloo.Service.extractors.register([
 			if(ctx.document.contentType.match(/^image/))
 				ctx.title = ctx.href.split('/').pop();
 			return download(itemUrl, getTempDir(), false).addCallback(function(file){
-				if(ctx.onLink && ctx.linkURL.match(illustURL) && !ctx.href.match(illustURL)){
-					return request(ctx.linkURL).addCallback(function(res){
+				if(ctx.onLink && ctx.link.href.match(illustURL) && !ctx.href.match(illustURL)){
+					return request(ctx.link.href).addCallback(function(res){
 						ctx.title = $x('//title/text()',convertToHTMLDocument(res.responseText));
 						return {
 							type	: 'photo',
 							item	: ctx.title,
 							itemUrl : itemUrl,
-							pageUrl : ctx.linkURL,
+							pageUrl : ctx.link.href,
 							file	: file,
 						}
 					});
